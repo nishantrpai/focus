3cb1140e4d2bdb170153a807a926c4c035b963bf
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-01-09 19:22:42 +0530

    update popup.js

--- src/popup.js
+++ src/popup.js
@@ -6,23 +6,19 @@ import './popup.css';
 (function () {
 
   let timer = '';
-  let startTime = '';
 
   function startTimer() {
     timer = setInterval(function () {
       let now = new Date();
-      let diff = now - new Date(startTime);
-      let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      let months = Math.floor(days / 31);
-      let years = Math.floor(months / 12);
-      days = days % 31;
-      months = months % 12;
-      let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-      let time = `${hours > 9 ? hours : '0' + hours}:${minutes > 9 ? minutes : '0' + minutes}:${seconds > 9 ? seconds : '0' + seconds}`
-      console.log(time);
-      document.getElementById('timer').innerHTML = time;
+      chrome.storage.local.get(['startTime']).then((result) => {
+        let startTime = result.startTime || new Date();
+        let diff = now - new Date(startTime);
+        let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        let time = `${hours > 9 ? hours : '0' + hours}:${minutes > 9 ? minutes : '0' + minutes}:${seconds > 9 ? seconds : '0' + seconds}`
+        document.getElementById('timer').innerHTML = time;
+      });
     }, 1000);
   }
 
@@ -36,11 +32,10 @@ import './popup.css';
         console.log('focus mode on');
         chrome.storage.local.set({ focusMode: true }).then(() => {
           // start counter
-          startTime = new Date();
           chrome.storage.local.set({ startTime: new Date() }).then(() => {
             console.log('start time is set');
+            if (!timer) startTimer();
           });
-          if(!timer) startTimer();
         });
       } else {
         chrome.storage.local.set({ focusMode: false }).then(() => {
@@ -59,19 +54,16 @@ import './popup.css';
   function restoreCounter() {
     console.log('restoreCounter');
     let focusMode = false;
-    chrome.storage.local.get(['startTime']).then((result) => {
-      startTime = result.startTime || new Date();
-    });
     chrome.storage.local.get(['focusMode']).then((result) => {
       focusMode = result.focusMode || false;
+      if (focusMode === true) {
+        document.getElementById('toggle').checked = true;
+        startTimer();
+      } else {
+        document.getElementById('toggle').checked = false;
+      }
+      setupCounter();
     });
-    if (focusMode === true) {
-      document.getElementById('toggle').checked = true;
-      startTimer();
-    } else {
-      document.getElementById('toggle').checked = false;
-    }
-    setupCounter();
   }
 
   document.addEventListener('DOMContentLoaded', restoreCounter);
